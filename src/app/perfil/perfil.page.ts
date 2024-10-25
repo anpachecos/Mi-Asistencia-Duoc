edbdcee6c12ba1e709639e4db4f11de062604467
anpachecos <an.pachecos@duocuc.cl>
2024-10-25 16:48:29 -0300

    llamamos al nombre del usuario desde el storage

--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StorageService } from '../services/storage.service';
 
 @Component({
   selector: 'app-perfil',
@@ -7,11 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilPage implements OnInit {
   profileImageUrl: string = '';
+  nombreUsuario: string = ''; // Variable para almacenar el nombre del usuario
 
-  constructor() {}
+  constructor(private storageService: StorageService) {}
 
-  ngOnInit() {
+  async ngOnInit() {
     this.getRandomPokemonImage();
+    await this.cargarNombreUsuario(); // Cargar el nombre del usuario logueado
   }
 
   // Método para obtener una imagen de Pokémon aleatoria
@@ -24,5 +27,11 @@ export class PerfilPage implements OnInit {
     } catch (error) {
       console.error("Error al obtener la imagen de Pokémon:", error);
     }
+  }
+
+  // Método para cargar el nombre del usuario logueado
+  async cargarNombreUsuario() {
+    await this.storageService.init(); // Asegura que el storage esté inicializado
+    this.nombreUsuario = await this.storageService.get('usuarioActivo') || ''; // Obtener el nombre del usuario
   }
 }
