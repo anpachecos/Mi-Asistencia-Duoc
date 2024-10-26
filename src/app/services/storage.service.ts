b331d9f412a8c6ccc4d0c70ab94956a01ce84830
anpachecos <an.pachecos@duocuc.cl>
2024-10-26 13:15:28 -0300

    Recuperar contraeña y log en la consola para ver los usuarios

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -13,19 +13,23 @@ export class StorageService {
     const storage = await this.storage.create();
     this._storage = storage;
 
-    // Gracias a esto podemos sobreescribir los usuarios cada vez que se inicie la app. 
-    const usuarios = [
-      { nombre: 'Antonia', password: '12345', apellido: 'Pacheco', carrera: 'Ingeniería en Informática' },
-      { nombre: 'Paolo', password: '881188', apellido: 'Rossi', carrera: 'Administración de Empresas' },
-      { nombre: 'Juan', password: 'abcde', apellido: 'García', carrera: 'Contabilidad' },
-      { nombre: 'Javier', password: '1234', apellido: 'Martínez', carrera: 'Ingeniería Civil' },
-      { nombre: 'Constanza', password: '1234', apellido: 'López', carrera: 'Derecho' }
-    ];    
+    // Solo inicializa los usuarios si aún no existen en el almacenamiento
+    const usuariosExistentes = await this._storage.get('usuarios');
+    if (!usuariosExistentes) {
+      const usuarios = [
+        { nombre: 'Antonia', password: '12345', apellido: 'Pacheco', carrera: 'Ingeniería en Informática' },
+        { nombre: 'Paolo', password: '881188', apellido: 'Rossi', carrera: 'Administración de Empresas' },
+        { nombre: 'Juan', password: 'abcde', apellido: 'García', carrera: 'Contabilidad' },
+        { nombre: 'Javier', password: '1234', apellido: 'Martínez', carrera: 'Ingeniería Civil' },
+        { nombre: 'Constanza', password: '1234', apellido: 'López', carrera: 'Derecho' }
+      ];
+      await this._storage.set('usuarios', usuarios);
+    }
 
-    // Guardamos la lista de usuarios
-    await this._storage.set('usuarios', usuarios);
+    // Imprimir la lista de usuarios actual en la consola
+    const usuarios = await this._storage.get('usuarios');
+    console.log('Usuarios actuales:', usuarios);
   }
-
 
   public async getUserByName(nombre: string) {
     await this.init();
@@ -37,18 +41,16 @@ export class StorageService {
       } else {
         throw new Error('Usuario no encontrado');
       }
-    } else {
+    } else {  
       throw new Error('No se pudo acceder a la lista de usuarios');
     }
   }
-
-//Lo de aquí para abajo lo copié de https://github.com/ionic-team/ionic-storage
 
   public async set(key: string, value: any) {
     await this.init(); 
     return this._storage?.set(key, value);
   }
-  
+
   public async get(key: string) {
     await this.init(); 
     return this._storage?.get(key);
