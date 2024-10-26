2525311b00f9b5cf2cb0edf2ead3080e4e1eb9bd
anpachecos <an.pachecos@duocuc.cl>
2024-10-26 12:55:26 -0300

    Ya se ven todos los datos del usuario en el perfil

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -13,34 +13,49 @@ export class StorageService {
     const storage = await this.storage.create();
     this._storage = storage;
 
-    // Sobrescribe o crea la lista de usuarios en cada inicio
+    // Gracias a esto podemos sobreescribir los usuarios cada vez que se inicie la app. 
     const usuarios = [
-      { nombre: 'Antonia', password: '12345' },
-      { nombre: 'Paolo', password: '881188' },
-      { nombre: 'Juan', password: 'abcde' },
-      { nombre: 'Javier', password: '1234' },
-      { nombre: 'Constanza', password: '1234' }
-    ];
+      { nombre: 'Antonia', password: '12345', apellido: 'Pacheco', carrera: 'Ingeniería en Informática' },
+      { nombre: 'Paolo', password: '881188', apellido: 'Rossi', carrera: 'Administración de Empresas' },
+      { nombre: 'Juan', password: 'abcde', apellido: 'García', carrera: 'Contabilidad' },
+      { nombre: 'Javier', password: '1234', apellido: 'Martínez', carrera: 'Ingeniería Civil' },
+      { nombre: 'Constanza', password: '1234', apellido: 'López', carrera: 'Derecho' }
+    ];    
 
-    // Guardar la lista de usuarios en el almacenamiento
+    // Guardamos la lista de usuarios
     await this._storage.set('usuarios', usuarios);
   }
 
-  // Guardar un valor
-  public async set(key: string, value: any) {
-    await this.init(); // Aseguramos que Storage esté listo
-    return this._storage?.set(key, value);
+
+  public async getUserByName(nombre: string) {
+    await this.init();
+    const usuarios = await this._storage?.get('usuarios');
+    if (usuarios) {
+      const usuarioEncontrado = usuarios.find((usuario: any) => usuario.nombre === nombre);
+      if (usuarioEncontrado) {
+        return usuarioEncontrado;
+      } else {
+        throw new Error('Usuario no encontrado');
+      }
+    } else {
+      throw new Error('No se pudo acceder a la lista de usuarios');
+    }
   }
 
-  // Obtener un valor
+//Lo de aquí para abajo lo copié de https://github.com/ionic-team/ionic-storage
+
+  public async set(key: string, value: any) {
+    await this.init(); 
+    return this._storage?.set(key, value);
+  }
+  
   public async get(key: string) {
-    await this.init(); // Aseguramos que Storage esté listo
+    await this.init(); 
     return this._storage?.get(key);
   }
 
-  // Eliminar un valor
   public async remove(key: string) {
-    await this.init(); // Aseguramos que Storage esté listo
+    await this.init(); 
     return this._storage?.remove(key);
   }
 }
